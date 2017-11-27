d6f48f281ad8731012b2189fc43200fff580a2f2
Jun Terauchi <j_terauchi@msc-inc.co.jp>
2017-11-27 23:18:25 +0900

    mod

--- src/main/java/co/jp/mscg/spark/examples/JavaSparkSQLExample.java
+++ src/main/java/co/jp/mscg/spark/examples/JavaSparkSQLExample.java
@@ -3,17 +3,24 @@ package co.jp.mscg.spark.examples;
 import static org.apache.spark.sql.functions.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.MapFunction;
 import org.apache.spark.sql.AnalysisException;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Encoder;
 import org.apache.spark.sql.Encoders;
 import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
 
 public class JavaSparkSQLExample {
 
@@ -45,6 +52,8 @@ public class JavaSparkSQLExample {
 
 		runBasicDataFrameExample(spark);
 		runDatasetCreationExample(spark);
+		runInferSchemaExample(spark);
+		runProgrammaticSchemaExample(spark);
 		// 最後はセッションを破棄しておく
 		spark.stop();
 
@@ -133,5 +142,60 @@ public class JavaSparkSQLExample {
 				});
 
 		// Apply a schema to an RDD of JavaBeans to get a DataFrame
+		Dataset<Row> peopleDF = spark.createDataFrame(peopleRDD, Person.class);
+		// Register the DataFrame as a temporary view
+		peopleDF.createOrReplaceTempView("people");
+
+		// SQL statements can be run by using the sql methods provided by spark
+		Dataset<Row> teenagersDF = spark.sql("SELECT name FROM people WHERE age BETWEEN 13 AND 19");
+
+		// The columns of a row in the result can be accessed by field index
+		Encoder<String> stringEncoder = Encoders.STRING();
+		Dataset<String> teenagerNamesByIndexDF = teenagersDF.map((MapFunction<Row, String>) row -> "Name: " + row.getString(0), stringEncoder);
+		teenagerNamesByIndexDF.show();
+
+		// or by field name
+		Dataset<String> teenagerNamesByFieldDF = teenagersDF.map((MapFunction<Row, String>) row -> "Name: " + row.<String>getAs("name"), stringEncoder);
+		teenagerNamesByFieldDF.show();
+
+
+	}
+
+	private static void runProgrammaticSchemaExample(SparkSession spark) {
+		// Create an RDD
+		JavaRDD<String> peopleRDD = spark.sparkContext()
+				.textFile("./data/people.txt", 1)
+				.toJavaRDD();
+
+		// The schema is encoded in a string
+		String schemaString = "name age";
+
+		// Generate the schema based on the string of schema
+		List<StructField> fields = new ArrayList<>();
+		for (String fieldName : schemaString.split(" ")) {
+			StructField field = DataTypes.createStructField(fieldName, DataTypes.StringType, true);
+			fields.add(field);
+		}
+		StructType schema = DataTypes.createStructType(fields);
+
+		// Convert records of the RDD (people) to Rows
+		JavaRDD<Row> rowRDD = peopleRDD.map((Function<String, Row>) record -> {
+			String[] attributes = record.split(" ");
+			return RowFactory.create(attributes[0], attributes[1].trim());
+		});
+
+		// Apply the schema to the RDD
+		Dataset<Row> peopleDataFrame = spark.createDataFrame(rowRDD, schema);
+
+		// Creates a temporary view using the DataFrame
+		peopleDataFrame.createOrReplaceTempView("people");
+
+		// SQL can be run over a temporary view created using DataFrame
+		Dataset<Row> results = spark.sql("SELECT name FROM people");
+
+		// The results of SQL queries are DataFrames and support all the normal RDD operations
+		// The columns of a row in the result can be accessed by field index or by field name
+		Dataset<String> namesDS = results.map((MapFunction<Row, String>) row -> "Name: " + row.getString(0), Encoders.STRING());
+		namesDS.show();
 	}
 }
